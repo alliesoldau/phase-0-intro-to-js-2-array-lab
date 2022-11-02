986f852f668e8a84646dab198d2d5f59160c6a68
Allie Soldau <alliesoldau@gmail.com>
2022-11-02 17:32:12 -0400

    final submit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const appendCatArray = [...cats, name];
+    return appendCatArray; 
+}
+
+function prependCat(name) {
+    const prependCatArray = [name, ...cats];
+    return prependCatArray;
+}
+
+function removeLastCat() {
+    const removeLastCatArray = cats.slice(0, cats.length - 1);
+    return removeLastCatArray;
+}
+
+function removeFirstCat() {
+    const removeFirstCatArray = cats.slice(1);
+    return removeFirstCatArray;
+}
